05441c176e3e8a82f20d02d59513dc73242c1e28
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-11-09 16:00:18 -0600

    Remove bootstrap-sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,18 +2,8 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var options = {
-    sassOptions: {
-      includePaths: [
-        'bower_components/bootstrap-sass/assets/stylesheets'
-      ]
-    }
-  };
+  var options = {};
   var app = new EmberApp(defaults, options);
-
-  // tooltips, popovers
-  // app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
-  app.import('bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss');
 
   return app.toTree();
 };
