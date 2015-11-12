26ca8ef40e80dd1c743fab0d11d23b5eadfae2b0
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-11-12 11:28:53 -0600

    Add demo table with clickable headers

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -2,5 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     application: Ember.inject.controller(),
-    queryParams: ['testParam']
+    sortAscending: true,
+    sortBy: null,
+    queryParams: ['sortBy', 'sortAscending', 'testParam'],
+    tableContent: Ember.A([
+      { startDate: '2015-11-10', endDate: '2016-11-10' },
+      { startDate: '2015-10-15', endDate: '2015-11-15' }
+    ]),
+    actions: {
+      //
+      // The setSortBy function receives the name of the function and
+      // toggle `sortAscending`. The function `toggleProperty` comes
+      // from the [Observable Mixin](http://emberjs.com/api/classes/Ember.Observable.html).
+      // It switches a boolean property between false and true.
+      //
+      setSortBy: function(fieldName) {
+        this.set('sortBy', fieldName);
+        this.toggleProperty('sortAscending');
+        console.log('Sorting by ', fieldName);
+        console.log('Sorting Asc?: ', this.get('sortAscending'));
+
+        return false;
+      }
+   }
+
 });
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 import { CanMixin } from 'ember-can';
 
 export default Ember.Route.extend(CanMixin, {
+  queryParams: {
+    sortBy: {
+      replace: true
+    },
+    sortAscending: {
+      replace: true
+    }
+  },
   beforeModel(transition) {
     const routeName = transition.targetName;
     const canAccess = this.can('access route', {routeName});
